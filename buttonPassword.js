b9e64509eca3a28fa314580f923d5683c772327b
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-07-15 21:43:00 -0500

    password finished

--- buttonPassword.js
+++ buttonPassword.js
@@ -1,21 +1,26 @@
-function passwordInput() {
-    let input
-    input = prompt("Enter the password.");
-    if (input == "p1n34pp13") {
-        introNav.class = "nav-link";
-        eidolonNav.class = "nav-link";
-        undertowNav.class = "nav-link";
-        passwordButton.class = "btn btn-outline-success disabled";
+let input = ""
+
+function passwordStorage() {
+    document.getElementById(input) == sessionStorage.input;
+    if (sessionStorage.input == "p1n34pp13") {
+        introNav.classList.remove("disabled");
+        eidolonNav.classList.remove("disabled");
+        undertowNav.classList.remove("disabled");
+        passwordButton.classList.add("disabled");
         hiddenTitle.style.visibility = "visible";
         hiddenText.style.visibility = "visible";
+        return input;
     } else {
-        introNav.class = "nav-link disabled";
-        eidolonNav.class = "nav-link disabled";
-        undertowNav.class = "nav-link disabled";
-        passwordButton.class = "btn btn-outline-success";
         hiddenTitle.style.visibility = "hidden";
         hiddenText.style.visibility = "hidden";
+        return input;
     }
 }
 
+function passwordInput() {
+    sessionStorage.input = prompt("Enter the password.");
+    passwordStorage();
+}
+
+document.body.onload = passwordStorage;
 passwordButton.onclick = passwordInput; 
