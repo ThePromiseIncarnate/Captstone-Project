d926f29d70b5890b1ee356d7fc598d52a8f9f86a
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-07-15 20:52:43 -0500

    okay now the password actually works

--- buttonPassword.js
+++ buttonPassword.js
@@ -1,16 +1,21 @@
-function passwordInput(){
-    let input = "";
-    input.password = prompt("Enter the password."); 
-    if (input.password = "p1n34pp13") {
+function passwordInput() {
+    let input
+    input = prompt("Enter the password.");
+    if (input == "p1n34pp13") {
         introNav.class = "nav-link";
         eidolonNav.class = "nav-link";
         undertowNav.class = "nav-link";
-        passwordButton.class = "btn btn-outline-success disabled"; 
+        passwordButton.class = "btn btn-outline-success disabled";
         hiddenTitle.style.visibility = "visible";
         hiddenText.style.visibility = "visible";
-   } else {
-
-   }
+    } else {
+        introNav.class = "nav-link disabled";
+        eidolonNav.class = "nav-link disabled";
+        undertowNav.class = "nav-link disabled";
+        passwordButton.class = "btn btn-outline-success";
+        hiddenTitle.style.visibility = "hidden";
+        hiddenText.style.visibility = "hidden";
+    }
 }
 
 passwordButton.onclick = passwordInput; 
